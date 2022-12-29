d65cc9fe9407d361dc75e5f8588fbd79f76fe57b
Cristian Massaneiro <96593642+crismassaneiro@users.noreply.github.com>
2022-12-29 14:49:22 -0300

    Atualização de pastas e criação de exercises

---
+++ W3Python/1-syntax.py
@@ -0,0 +1,19 @@
+#Insert the missing part of the code below to output "Hello World".
+
+#("Hello World")
+
+print ("Hello World")
+
+#This example misses indentations to be correct.
+#Insert the missing indentation to make the code correct:
+
+
+#Está faltando espaço no print (python é chato nessa parte, tem que cuidar o espaçam,ento)
+
+#Errado
+'''if 5 > 2:
+print("Five is greater than two!")'''
+
+#Certo 
+if 5 > 2:
+    print("Five is greater than two!")
